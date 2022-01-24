7ba0dd1ed2bef091d32e48f260649a6bfabdbeea
nnikka <n.kurashvili@sharemac.de>
2022-01-24 13:05:34 +0400

    create export architecture for component sub dir

---
+++ projects/sh-library/src/lib/components/public-api.ts
@@ -0,0 +1,2 @@
+export * from './sh-input/sh-input.module';
+export * from './sh-select/sh-select.module';
--- projects/sh-library/src/public-api.ts
+++ projects/sh-library/src/public-api.ts
@@ -1,3 +1,4 @@
 /*
  * Public API Surface of sh-library
  */
+export * from './lib/components';

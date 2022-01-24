96237f64d883b9164ff42bf61d02917eec27e120
nnikka <n.kurashvili@sharemac.de>
2022-01-24 15:33:32 +0400

    create secondary entry point for input

---
+++ projects/sh-library/input/src/input.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InputComponent } from './components/input/input.component'
+
+@NgModule({
+    declarations: [InputComponent],
+    imports: [
+        CommonModule
+    ],
+    exports: [InputComponent]
+})
+export class InputModule { }
--- projects/sh-library/src/public-api.ts
+++ projects/sh-library/src/public-api.ts
@@ -1,6 +1,4 @@
 /*
  * Public API Surface of sh-library
  */
-export * from './lib/components/sh-input/sh-input.module';
-export * from './lib/components/sh-input/input/input.component';
-export * from './lib/components/sh-select/sh-select.module';
+export { };

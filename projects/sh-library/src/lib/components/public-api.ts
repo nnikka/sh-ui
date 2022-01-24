7efaa9017d89d07b85a2a3e693ccdf73690dcc42
nnikka <n.kurashvili@sharemac.de>
2022-01-24 13:22:03 +0400

    remove sub entry point for components

--- projects/sh-library/src/lib/components/public-api.ts
+++
@@ -1,2 +0,0 @@
-export * from './sh-input/sh-input.module';
-export * from './sh-select/sh-select.module';
--- projects/sh-library/src/public-api.ts
+++ projects/sh-library/src/public-api.ts
@@ -1,4 +1,6 @@
 /*
  * Public API Surface of sh-library
  */
-export * from './lib/components';
+export * from './lib/components/sh-input/sh-input.module';
+export * from './lib/components/sh-input/input/input.component';
+export * from './lib/components/sh-select/sh-select.module';
